0e2c3d9172490cbe83642497f86b9163a31f8a9c
Noah Söng <noah.song@elev.ga.ntig.se>
2024-03-06 18:48:21 +0100

    Fixat JSON och LocalStorage

--- javascript.js
+++ javascript.js
@@ -1,48 +1,61 @@
-var varukorg = []; //Varukorg array
+var varukorg = []; // Varukorg array
 
-function läggTillVarukorg(namn, pris) 
-    {
-        varukorg.push({ namn: namn, pris: pris });
-        visaVarukorg(); // Anropa visaVarukorg() varje gång en produkt läggs till
+function läggTillVarukorg(namn, pris) {
+    varukorg.push({ namn: namn, pris: pris });
+    visaVarukorg(); // Anropa visaVarukorg() varje gång en produkt läggs till
+    sparaVarukorgILocalStorage(); // Spara varukorgen när en produkt läggs till
+}
+
+function visaVarukorg() {
+    var modal = document.getElementById("varukorg-modal");
+    var varukorgLista = document.getElementById("varukorg-lista");
+
+    // Rensa varukorgslistan innan du lägger till produkterna igen
+    varukorgLista.innerHTML = "";
+
+    // Loopa igenom varukorgen och skapa text för varje produkt
+    var totalSumma = 0;
+    for (var i = 0; i < varukorg.length; i++) {
+        var produkt = varukorg[i];
+        var listItem = document.createElement("li");
+        listItem.textContent = produkt.namn + " - " + produkt.pris + " KR";
+        varukorgLista.appendChild(listItem);
+
+        //Räkna ihop summan av alla produkter i varukorgen
+        totalSumma += produkt.pris;
     }
 
-function visaVarukorg() 
-    {
-        var modal = document.getElementById("varukorg-modal");
-        var varukorgLista = document.getElementById("varukorg-lista");
+    var totalSummaElement = document.createElement("li");
+    totalSummaElement.textContent = "Totalt: " + totalSumma + " KR";
+    varukorgLista.appendChild(totalSummaElement);
 
-        // Rensa varukorgslistan innan du lägger till produkterna igen
-        varukorgLista.innerHTML = "";
+    modal.style.display = "block"; // Visa varukorg-modalen 
+}
 
-        // Loopa igenom varukorgen och skapa text för varje produkt
-        var totalSumma = 0;
-            for (var i = 0; i < varukorg.length; i++) 
-                {
-                    var produkt = varukorg[i];
-                    var listItem = document.createElement("li");
-                    listItem.textContent = produkt.namn + " - " + produkt.pris + " KR";
-                    varukorgLista.appendChild(listItem);
+function stängVarukorg() {
+    var modal = document.getElementById("varukorg-modal");
+    modal.style.display = "none"; // Göm varukorg-modalen
+}
 
-                    //Räkna ihop summan av alla produkter i varukorgen
-                    totalSumma += produkt.pris;
-                }
+function återställVarukorg() {
+    varukorg = [];
+    visaVarukorg();
+    sparaVarukorgILocalStorage(); // Spara varukorgen när den återställs
+}
 
-        var totalSummaElement = document.createElement("li");
-        totalSummaElement.textContent = "Totalt: " + totalSumma + " KR";
-        varukorgLista.appendChild(totalSummaElement);
+function sparaVarukorgILocalStorage() {
+    const varukorgJSON = JSON.stringify(varukorg); // Konvertera varukorgen till en JSON-sträng
+    localStorage.setItem('varukorg', varukorgJSON); // Spara varukorgen i localStorage
+}
 
-        modal.style.display = "block"; // Visa varukorg-modalen 
+// Ladda varukorgen från localStorage när sidan laddas
+function laddaVarukorgFrånLocalStorage() {
+    const varukorgJSON = localStorage.getItem('varukorg'); // Hämta varukorgen från localStorage
+    if (varukorgJSON) {
+        varukorg = JSON.parse(varukorgJSON); // Konvertera JSON-strängen tillbaka till en JS-array
+        visaVarukorg(); // Visa varukorgen på sidan
     }
+}
 
-function stängVarukorg() 
-    {
-        var modal = document.getElementById("varukorg-modal");
-        modal.style.display = "none"; // Göm varukorg-modalen
-    }
-
-function återställVarukorg() 
-    {    
-        varukorg = [];
-        visaVarukorg();
-    }
-    
+// Anropa funktionen för att ladda varukorgen från localStorage när sidan laddas
+laddaVarukorgFrånLocalStorage();
